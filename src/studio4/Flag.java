acccdbdafb2278b0a04d95e1bdcacd9381513192
18566 <18566@CaitlinsDell.dhcp.wustl.edu>
2025-02-19 14:18:01 -0600

    finished studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color lightBlue = new Color (114, 160, 193);
+		Color lighterBlue = new Color (128, 218, 235);
+		StdDraw.setPenColor(lightBlue);
+		StdDraw.filledRectangle(0.5, 0.5, 1, 1);
+		StdDraw.setPenRadius(0.25);
+		StdDraw.setPenColor(lighterBlue);
+		StdDraw.line(0, 0.5, 1, 0.5);
+	//	StdDraw.setPenColor(Color.YELLOW);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,50 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		System.out.println(shape);
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		StdDraw.setPenColor(red, green, blue);
+		boolean filled = in.nextBoolean();
+		if (shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double [] xTriangle =  {x1, x2, x3};
+			double [] yTriangle = {y1, y2, y3};
+			
+			if (filled) {
+				StdDraw.filledPolygon(xTriangle, yTriangle);
+			} else {
+				StdDraw.polygon(xTriangle, yTriangle);
+			}
+		} else {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			if (shape.equals("rectangle")) {
+				double halfWidth = in.nextDouble();
+				double halfHeight = in.nextDouble();
+				if (filled) {
+					StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+				} else {
+					StdDraw.rectangle(x, y, halfWidth, halfHeight);
+				}
+			}
+			if (shape.equals("ellipse")) {
+				double semiMajor = in.nextDouble();
+				double semiMinor = in.nextDouble();
+				if (filled) {
+					StdDraw.filledEllipse(x, y, semiMajor, semiMinor);
+				} else {
+					StdDraw.ellipse(x, y, semiMajor, semiMinor);
+				}
+			}
+
+		}
 	}
 }
